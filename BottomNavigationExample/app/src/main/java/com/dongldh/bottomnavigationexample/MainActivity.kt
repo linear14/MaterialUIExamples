bb035ceef071c565ab46a86c00cafbab24820ea3
LeeDongHyun <linear14@naver.com>
2020-08-18 14:36:14 +0900

    BottomNavigation #001.Basic
    Basic BottomNavigation

---
+++ BottomNavigationExample/app/src/main/java/com/dongldh/bottomnavigationexample/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.dongldh.bottomnavigationexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        basic.setOnClickListener { startActivity(Intent(this, BasicActivity::class.java)) }
+    }
+}
